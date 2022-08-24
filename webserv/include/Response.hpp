385f7e618bf87abd25dc23eb0adf2d916529aa12
kimhxsong <kimhxsong@gmail.com>
2022-08-24 20:52:30 +0900

    Merge remote-tracking branch 'origin/develop'

---
+++ webserv/include/Response.hpp
@@ -0,0 +1,49 @@
+#ifndef RESPONSE_HPP
+#define RESPONSE_HPP
+
+#include <map>
+#include <sstream>
+#include <string>
+
+class Response
+{
+public:
+	typedef std::map<int, std::string> statusInfoType;
+	typedef std::map<std::string, std::string> headerType;
+
+private:
+	static statusInfoType defaultInfo;
+	int statusCode;
+	headerType header;
+	std::stringstream body;
+	std::string buffer;
+	std::size_t sentBytes;
+	std::size_t totalBytes;
+	bool isReady;
+
+public:
+	Response();
+	Response(const Response &other);
+	~Response();
+
+	bool ready() const;
+	bool done() const;
+	void clear();
+
+	const char *getBuffer() const;
+	std::size_t getBufSize() const;
+	std::size_t moveBufPosition(int nbyte);
+
+	std::stringstream &getBodyStream();
+	void setStatusCode(int code);
+	void setHeader(std::string name, std::string value);
+	void setBuffer();
+
+private:
+	Response &operator=(const Response &rhs);
+
+	static statusInfoType initializeDefaultInfo();
+	std::string getStatusInfo(int code) const;
+};
+
+#endif // !RESPONSE_HPP
