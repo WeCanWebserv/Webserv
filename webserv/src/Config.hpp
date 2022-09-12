689015ca1153314adf3e84a16f5fbaee85b90afb
kimhxsong <kimhxsong@gmail.com>
2022-09-12 16:51:59 +0900

    chore: add comments

--- webserv/src/Config.hpp
+++ webserv/src/Config.hpp
@@ -18,8 +18,23 @@ struct LocationConfig
 	std::pair<int, std::string> redirectionSetting;
 	std::set<std::string> allowedMethods;
 	bool isAutoIndexOn;
-	bool isRedirectionSet;
+	bool isRedirectionSet;  // 'return' 지시어 중복 방지
 };
+// Example 1:
+// location /download {
+//      root html;
+//      index index.html index.htm;
+//      autoindex on;  // default = off
+//      cgi_bin .php /usr/bin/php;
+//      cgi_upload .php /usr/local/download;
+//      allowed_methods GET POST DELETE;  // default = GET
+// }
+
+// Example 2:
+// location /redir {
+//     return 301 /usr/upload;
+// }
+
 
 struct ServerConfig
 {
@@ -30,6 +45,21 @@ struct ServerConfig
 	int listennedPort;
 	int maxRequestBodySize;
 };
+// Example 1:
+// server {
+//     listen 2130706433 80; <-- To Fix
+//     server_name example.com;
+
+//     location {...} {
+//        {...}
+//     }
+//     location {...} {
+//        {...}
+//     }
+// 
+//     max_request_body_size 10240;
+//     error_page 500 502 503 504  /50x.html;
+// }
 
 struct Config
 {
