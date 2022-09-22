f9e11d2ced28889dffe8c90a5f9b6c1a20e041ae
kimhxsong <kimhxsong@gmail.com>
2022-09-22 19:20:20 +0900

    #29 chore: move function-definitions to source from header

--- webserv/src/ConfigParser.cpp
+++ webserv/src/ConfigParser.cpp
@@ -12,6 +12,9 @@
 #include "libft.hpp"
 
 //
+// class ConfigParser
+//
+
 // public:
 //
 ConfigParser::ConfigParser(const char *path) : configFile(path)
@@ -31,7 +34,6 @@ ConfigParser::~ConfigParser()
 	this->configFile.close();
 }
 
-//
 // protected:
 //
 void ConfigParser::fillBufferExceptComment(std::ifstream &inputStream,
@@ -118,7 +120,6 @@ void ConfigParser::handleLineInServerBlock(ConfigBlock &configBlock, std::string
 		{
 			std::string uri;
 			std::getline(lineBuffer, uri, ' ');
-			// if (uri.empty() || uri.front() != '/')
 			if (uri.empty() || uri[0] != '/')
 				throw std::runtime_error("syntax wrong: context location");
 
@@ -209,7 +210,6 @@ void ConfigParser::populate()
 	ConfigBlock configBlock;
 	for (size_t i = 0; i < lines.size(); i++)
 	{
-		// TOOD
 		std::string &line = lines[i];
 		switch (configBlock.context)
 		{
@@ -225,4 +225,222 @@ void ConfigParser::populate()
 		}
 	}
 	this->config = configBlock.toConfig();
+}
+
+//
+// struct LocationBlock
+//
+LocationBlock::LocationBlock() : isInBlock(false)
+{
+	std::map<std::string, int> &kindOf = this->kindOf;
+
+	kindOf["root"] = LOC_ROOT;
+	kindOf["index"] = LOC_INDEX;
+	kindOf["autoindex"] = LOC_AUTOINDEX;
+	kindOf["return"] = LOC_REDIRECTION;
+	kindOf["allowed_methods"] = LOC_ALLOWED_METHODS;
+	kindOf["cgi_bin"] = LOC_CGI_BIN;
+	kindOf["cgi_upload"] = LOC_CGI_UPLOAD;
+
+	availableMethods.insert(std::string("GET"));
+	availableMethods.insert(std::string("POST"));
+	availableMethods.insert(std::string("DELETE"));
+}
+
+LocationConfig LocationBlock::toLocationConfig()
+{
+	LocationConfig locationConfig;
+	for (size_t i = 0; i < directives.size(); i++)
+	{
+		std::vector<std::string> tokens = ft_split(directives[i], ' ');
+		const std::string &directive = tokens[0];
+		if (this->kindOf.find(directive) == this->kindOf.end())
+			throw std::runtime_error("invalid location block directive");
+		switch (this->kindOf[directive])
+		{
+		case LOC_ROOT:
+		{
+			if (tokens.size() != 2)
+				throw std::runtime_error("invalid location block: root:");
+			locationConfig.root = tokens[1];
+			break;
+		}
+		case LOC_INDEX:
+		{
+			if (tokens.size() < 2)
+				throw std::runtime_error("invalid location block: root:");
+			if (!locationConfig.indexFiles.empty())
+				throw std::runtime_error("invalid location block: index: duplicated");
+			locationConfig.indexFiles.insert(locationConfig.indexFiles.begin(), ++tokens.begin(),
+																			 tokens.end());
+			break;
+		}
+		case LOC_AUTOINDEX:
+		{
+			if (tokens.size() != 2 || (tokens[1] != "on" && tokens[1] != "off"))
+				throw std::runtime_error("invalid location block: autoindex");
+			locationConfig.isAutoIndexOn = tokens[1] == "on" ? true : false;
+			break;
+		}
+		case LOC_REDIRECTION:
+		{
+			if (tokens.size() < 2 || tokens.size() > 3)
+				throw std::runtime_error("invalid location block: redirection");
+			std::stringstream buffer(tokens[1]);
+			int statusCode;
+			buffer >> statusCode;
+			if (!buffer.eof())
+				throw std::runtime_error("invalid location block: return:");
+			locationConfig.redirectionSetting.first = statusCode;
+			if (tokens.size() == 3)
+				locationConfig.redirectionSetting.second = tokens[2];
+			locationConfig.isRedirectionSet = true;
+			break;
+		}
+		case LOC_ALLOWED_METHODS:
+		{
+			if (tokens.size() < 2)
+				throw std::runtime_error("invalid location block: allowed_method:");
+			for (size_t i = 1; i < tokens.size(); i++)
+			{
+				if (this->availableMethods.find(tokens[i]) == this->availableMethods.end())
+					throw std::runtime_error("invalid location block: allowed_method:");
+				if (locationConfig.allowedMethods.insert(tokens[i]).second == false)
+					throw std::runtime_error("invalid location block: allowed_method:");
+			}
+			break;
+		}
+		case LOC_CGI_BIN:
+		{
+			if (tokens.size() != 3)
+				throw std::runtime_error("invalid location block: cgi_bin:");
+			if (tokens[1][0] != '.')
+				throw std::runtime_error("invalid location block: cgi_bin:");
+			if (locationConfig.tableOfCgiBins.insert(std::make_pair(tokens[1], tokens[2])).second ==
+					false)
+				throw std::runtime_error("invalid location block: cgi_bin: duplicated");
+			break;
+		}
+		case LOC_CGI_UPLOAD:
+		{
+			if (tokens.size() != 3)
+				throw std::runtime_error("invalid location block: cgi_upload:");
+			if (tokens[1][0] != '.')
+				throw std::runtime_error("invalid location block: cgi_upload:");
+			if (locationConfig.tableOfCgiBins.find(tokens[1]) == locationConfig.tableOfCgiBins.end())
+				throw std::runtime_error("invalid location block: cgi_upload:");
+			if (locationConfig.tableOfCgiUploads.insert(std::make_pair(tokens[1], tokens[2])).second ==
+					false)
+				throw std::runtime_error("invalid location block: cgi_upload: duplicated");
+		}
+		}
+	}
+	return locationConfig;
+}
+
+//
+// struct ServerBlock
+//
+ServerBlock::ServerBlock() : isInBlock(false)
+{
+	kindOf["server_name"] = SERV_SERVER_NAME;
+	kindOf["error_page"] = SERV_ERROR_PAGES;
+	kindOf["listen"] = SERV_LISTEN;
+	kindOf["max_request_body_size"] = SERV_MAX_REQUEST_BODY_SIZE;
+}
+
+ServerConfig ServerBlock::toServerConfig()
+{
+	ServerConfig serverConfig;
+	for (size_t i = 0; i < this->directives.size(); i++)
+	{
+		std::vector<std::string> tokens = ft_split(directives[i], ' ');
+		const std::string &directive = tokens[0];
+		if (this->kindOf.find(directive) == this->kindOf.end())
+			throw std::runtime_error("invalid server block directive");
+		switch (this->kindOf[directive])
+		{
+		case SERV_SERVER_NAME:
+		{
+			if (tokens.size() < 2)
+				throw std::runtime_error("invalied server block: server_name");
+			if (!serverConfig.listOfServerNames.empty())
+				throw std::runtime_error("invalied server block: server_name");
+			serverConfig.listOfServerNames.insert(serverConfig.listOfServerNames.begin(),
+																						++tokens.begin(), tokens.end());
+			break;
+		}
+		case SERV_ERROR_PAGES:
+		{
+			if (tokens.size() < 3)
+				throw std::runtime_error("invalied server block: error_page");
+			const std::string &page = tokens.back();
+			for (size_t i = 1; i < tokens.size() - 1; i++)
+			{
+				std::stringstream buffer(tokens[i]);
+				int statusCode;
+				buffer >> statusCode;
+				if (serverConfig.tableOfErrorPages.insert(std::make_pair(statusCode, page)).second == false)
+					throw std::runtime_error("invalied server block: error_page");
+			}
+			break;
+		}
+		case SERV_LISTEN:
+		{
+			if (tokens.size() != 3)
+				throw std::runtime_error("invalied server block: listen");
+			std::stringstream buffer;
+			buffer.str(tokens[1]);
+			int ipAddress;
+			buffer >> ipAddress;
+			if (!buffer.eof())
+				throw std::runtime_error("invalied server block: listen");
+			serverConfig.listennedHost = ipAddress;
+
+			buffer.clear();
+			int port;
+			buffer.str(tokens[2]);
+			buffer >> port;
+			if (!buffer.eof())
+				throw std::runtime_error("invalied server block: listen");
+			serverConfig.listennedPort = port;
+			break;
+		}
+		case SERV_MAX_REQUEST_BODY_SIZE:
+		{
+			std::stringstream buffer(tokens[1]);
+			int maxReqSize;
+			buffer >> maxReqSize;
+			if (!buffer.eof())
+				throw std::runtime_error("invalied server block: max_request_body_size");
+			serverConfig.maxRequestBodySize = maxReqSize;
+			break;
+		}
+		default:
+			break;
+		}
+		for (std::map<std::string, LocationBlock>::iterator it = this->locationBlocks.begin();
+				 it != this->locationBlocks.end(); it++)
+		{
+			serverConfig.tableOfLocations.insert(
+					std::make_pair(it->first, it->second.toLocationConfig()));
+		}
+	}
+	return serverConfig;
+};
+
+//
+// struct ConfigBlock
+//
+ConfigBlock::ConfigBlock() : isInBlock(false), context(CONTEXT_NON) {}
+
+Config ConfigBlock::toConfig()
+{
+	Config config;
+	for (std::vector<ServerBlock>::iterator it = this->serverBlocks.begin();
+			 it != this->serverBlocks.end(); it++)
+	{
+		config.serverConfigs.push_back(it->toServerConfig());
+	}
+	return config;
 }
